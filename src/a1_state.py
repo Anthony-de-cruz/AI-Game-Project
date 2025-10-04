f08779c65028f647807d307bed0d686b424c3a2c
MinusMason <masonfeb10@gmail.com>
2025-10-04 17:00:02 +0100

    Replaced header

--- src/a1_state.py
+++ src/a1_state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct  4 16:31:20 2025
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
 
-@author: masonbuckle
+Includes a State class for Task 1
+
+@author: A3 REPLACE WITH OUR STUDENT ID'S :(12345, 5678, and 89123)
+@date:   04/10/2025
 """
 
+class State:
+    size = 5
+    
+    def __init__(self,grid):
+        '''
+        Parameters
+        ----------
+        grid : 2D List of lists
+
+        '''
+        
+        # Check for error with assert
+        assert(len(grid) == State.size)
+        self.grid = grid
+        
+    def __str__(self):
+        return self.grid
