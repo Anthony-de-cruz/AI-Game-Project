e9835db2881600764a0d7f70127091a1a71e2b5b
MinusMason <masonfeb10@gmail.com>
2025-10-05 17:59:12 +0100

    Finished the num_regions method

--- src/a1_state.py
+++ src/a1_state.py
@@ -46,6 +46,43 @@ class State:
                     new_grid = self.clone() 
                     new_grid[i][j] -= 1
                     yield State(new_grid)
+                    
+    def numRegions(self):
+        '''
+        Returns
+        -------
+        Number of active regions on the board
+        '''
+        row = len(self.grid)
+        col = len(self.grid[0])
+        
+        visited = []
+        active_regions = 0
+        
+        # Loop over all cells
+        for i in range(row):
+            for j in range(col):
+                if self.grid[i][j] > 0 and (i,j) not in visited: # if active and not visited yet
+                    active_regions += 1
+                    stack = [(i,j)]
+                    
+                    #dfs
+                    while stack:
+                        a,b = stack.pop()
+                        if (a,b) not in visited:
+                            visited.append((a,b))
+                            # Find neighours of (a,b)
+                            for x in range(row):
+                                for y in range(col):
+                                    if (x,y) not in visited and self.grid[x][y] > 0: # if active and not visited
+                                        # max(∣a−x∣,∣b−y∣)=1 check for neighbouring cells
+                                        if max(abs(a-x), abs(b-y)) == 1:
+                                            stack.append((x,y))
+        return active_regions
+                            
+                    
+                
+        
            
 grid = [[1,1,0,2],
         [1,1,0,0],
@@ -53,7 +90,7 @@ grid = [[1,1,0,2],
         [0,0,1,1]]
 
 st = State(grid)
-
+print("The number of active regions is:", st.numRegions())
 print(f'The Original state is : \n{st}')
 print('\nThe next possible moves are : \n')
 for next_state in st.moves():
