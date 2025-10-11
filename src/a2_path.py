4215831647cd25a1c38a6694312e0bd152f48792
MinusMason <masonfeb10@gmail.com>
2025-10-11 16:30:46 +0100

    Created a2_path.py

---
+++ src/a2_path.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 2
+
+@author: A3 REPLACE WITH OUR STUDENT ID'S :(12345, 5678, and 89123)
+@date:   11/10/2025
+"""
+
