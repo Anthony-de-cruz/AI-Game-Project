cf9796bc4af77adf2573aee5cbd191b6b33b135d
MinusMason <masonfeb10@gmail.com>
2025-10-19 16:44:10 +0100

    Create a3_agent.py

---
+++ src/a3_agent.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+@author: A23 (100385770, 100428908, TBC)
+@date:   19/10/2025
+"""
+
