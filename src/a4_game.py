e24223de1c48e0caf8c7c030fbfc4c15861f2fab
MinusMason <masonfeb10@gmail.com>
2025-10-24 14:02:10 +0100

    Create a4_game.py

---
+++ src/a4_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+@author: A23 (100385770, 100428908, TBC)
+@date:   24/10/2025
+"""
+
